6e2a186b7d194afa7f787d54e99c33bde2de4f0f
NeBeWu <nebewu@gmail.com>
2022-07-12 20:26:59 -0300

    Associate event and user (as creator)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'events/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    # Redirects signing out users back to sign-in (necessary when deleting user)
+    get 'users', to: 'posts#index'
+  end
+
 
   # Defines the root path route ("/")
   root 'events#index'
