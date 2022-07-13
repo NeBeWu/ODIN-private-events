18daa974ab11c2fbec6467973fdea1b971700eda
NeBeWu <nebewu@gmail.com>
2022-07-13 01:32:54 -0300

    Add event show view and fix with event index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,12 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show]
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
-    @events = Event.all
+    @events = Event.all.includes([:creator])
   end
+
+  def show; end
 
   def new
     @new_event = current_user.events.build
@@ -19,6 +24,12 @@ class EventsController < ApplicationController
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
   def event_params
     params.require(:event).permit(:title, :description, :date, :location, :price)
   end
