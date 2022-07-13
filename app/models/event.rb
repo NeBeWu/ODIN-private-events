7192cbe6fa1e723075d1aa82308d01c77906c372
NeBeWu <nebewu@gmail.com>
2022-07-13 00:02:28 -0300

    Rework event price form field, add validation and set it's output to scale 2

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than: 10_000 }
 end
