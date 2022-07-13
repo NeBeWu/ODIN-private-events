f15957646cf411e2744359053091d9f04b62ac04
NeBeWu <nebewu@gmail.com>
2022-07-12 22:45:15 -0300

    Implement event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @new_event = current_user.events.build
+  end
+
+  def create
+    @new_event = current_user.events.build(event_params)
+
+    if @new_event.save
+      redirect_back_or_to :root, notice: 'Event was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :location, :price)
+  end
 end
