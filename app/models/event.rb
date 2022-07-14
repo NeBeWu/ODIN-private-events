f434c41101496aa2c8fdcff15954b869da6f51cf
NeBeWu <nebewu@gmail.com>
2022-07-13 23:55:20 -0300

    Implement attendance model with migration and associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+  belongs_to :creator, class_name: 'User'
+
+  has_many :attendances, foreign_key: 'attended_event_id', inverse_of: 'attended_event'
+  has_many :attendees, through: :attendances
 
   validates :price, numericality: { greater_than_or_equal_to: 0, less_than: 10_000 }
 end
