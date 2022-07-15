c2b41d33cd6cf39e374d17c473e5a2a5f4744182
NeBeWu <nebewu@gmail.com>
2022-07-15 17:57:35 -0300

    Refactor user code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update]
+  before_action :set_user, except: %i[index new create]
   before_action :authenticate_user!, except: %i[index show]
+  before_action :authorize_user, only: %i[edit update]
 
-  def show
-    @created_events = @user.created_events
-  end
+  def show; end
 
   def edit; end
 
@@ -12,13 +11,18 @@ class UsersController < ApplicationController
     @user.assign_attributes(user_params)
 
     if @user.save
-      redirect_to user_path(current_user.id), notice: 'Event was successfully created.'
+      redirect_to user_path(current_user.id), notice: 'Profile was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   private
+
+  # Redirects user in case it has not authorization to proceed
+  def authorize_user
+    redirect_to user_path(@user.id), alert: 'You are not allowed to do this.' unless @user == current_user
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
